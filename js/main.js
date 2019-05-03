06f3a1e5fdb2bcfd9366a024182db7abe17b96be
Casey Brannon <cbrannon412@gmail.com>
2019-05-03 16:59:53 -0700

    targetFunc/style

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
-const div = document.querySelector('div');
-const dip = document.cre
-div.addEventListener("click", function(evt){
-  console.log(evt.)
-});
+/*----- constants -----*/
+const targets = {
+  bg1: {
+    img: 'img/index.jpg',
+    pts: '10'
+  }
+
+}
+/*----- app's state (variables) -----*/
+var target1 = document.querySelector(".image");
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+document.querySelector(".image").addEventListener('click', kill)
+ 
+
+/*----- functions -----*/
+render()
+
+function kill(evt) {
+  var marker = evt.target;
+marker.style.display = "none";
+render()
+}
+
+function render() {
+target1.style.content = 'url(img/index.jpg)';
+}
+
+
+
+//const playerResultEl = document.querySelector('#player > div > div');
+//playerResultEl.style.backgroundImage = `url(${rps[results.p].img})`;
