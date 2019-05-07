f000da14cdf68e0831abaaff3878b620e9b10d2b
Casey Brannon <cbrannon412@gmail.com>
2019-05-07 16:02:38 -0700

    move logic

--- js/main.js
+++ js/main.js
@@ -4,17 +4,17 @@
 const reload = new Audio('https://freesound.org/data/previews/276/276956_5172328-lq.mp3')
 const gunShot = new Audio('https://freesound.org/data/previews/212/212607_1654571-lq.mp3');
 /*----- app's state (variables) -----*/
-var target1 = document.querySelector(".target");
+var target1 = document.getElementById("target");
 var timer1 = document.getElementById("timer");
 var start = document.getElementById("start");
 // var startGame = document.querySelector('button');
 var points = document.querySelector('.score');
 var fOv = document.querySelector('.level');
 var areas = document.getElementsByClassName('area')
-console.log(areas)
+
 
 /*----- cached element references -----*/
-var level, hit, miss, currentTarget
+var level, points, miss, currentTarget
 
 
 /*----- event listeners -----*/
@@ -30,33 +30,63 @@ init()
 
 function popUp() {
   var random = Math.floor((Math.random() * 6) + 0);
-  currentTarget = areas[random]
-  currentTarget.textContent = "HERE";
-
+  currentTarget = areas[random];
+  currentTarget = currentTarget.append(target1)
+  
+  //currentTarget.textContent = "HERE";
 }
 
 function removePopUp() {
   if (currentTarget = currentTarget) {
-    currentTarget.textContent = null;
+    currentTarget = null;
   }
-
-
 }
 //count down 
 
 function timer() {
-  var timeStart = 5;
+  var timeStart = 60;
   var gameTime = setInterval(function () {
     timer1.innerHTML = timeStart;
     timeStart -= 1;
     if (timeStart < 0) {
       clearInterval(gameTime);
-      init()
+      
     }
+    
   }, 1000)
+  
 }
 
-//gun  sound
+
+function hit(evt) {
+  var marker = evt.target;
+  var point = 0;
+  point = points;
+  if(marker === target1) {
+    return point =+ 1;
+    
+  }
+  console.log(marker);
+  
+}
+
+
+
+
+// button to start clock
+function render() {
+  var popUs = setInterval(function () {
+    popUp()
+  }, 1000)
+  var removePopUps = setInterval(function () {
+    removePopUp()
+  }, 900)
+  reload.play();
+  timer();
+  
+  }
+
+  //gun  sound
 
 function gunSound() {
   gunShot.play();
@@ -64,29 +94,8 @@ function gunSound() {
 }
 
 
-function hit(evt) {
-  var marker = evt.target;
-  console.log(marker);
-}
-
-function playGame() {
-  render()
-}
-
-// button to start clock
-function render() {
-  reload.play();
-  var popUs = setInterval(function () {
-    popUp()
-  }, 1000)
-  var removePopUps = setInterval(function () {
-    removePopUp()
-  }, 900)
-  timer();
-}
-
 function init() {
   currentTarget = [null, null, null, null, null, null];
-  points = 0;
-
+  
+  
 }
