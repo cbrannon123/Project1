f8659cd0dd0c22ab259d6ee77fe4dc9f307d8594
Casey Brannon <cbrannon412@gmail.com>
2019-05-09 16:14:28 -0700

    icebox

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 /*----- constants -----*/
-
-
+const grunt = new Audio('https://freesound.org/data/previews/369/369003_6812364-lq.mp3')
+const hitSound = new Audio('https://freesound.org/data/previews/166/166605_71257-lq.mp3')
+const winMusic = new Audio('https://freesound.org/data/previews/251/251419_970024-lq.mp3')
+const music = new Audio('https://freesound.org/data/previews/125/125185_45941-lq.mp3')
+const loser = new Audio('https://freesound.org/data/previews/77/77176_216886-lq.mp3')
 const reload = new Audio('https://freesound.org/data/previews/276/276956_5172328-lq.mp3')
 const gunShot = new Audio('https://freesound.org/data/previews/212/212607_1654571-lq.mp3');
 /*----- app's state (variables) -----*/
@@ -33,17 +36,20 @@ function winMsg() {
   msg.innerHTML = `YOU WIN with ${points.innerHTML} hits!`;
   target1.style.display = "none";
   timeStart = 2;
+  winMusic.play()
 
 }
 
 function timeUp() {
   msg.innerHTML = `Press start to play again!`;
   target1.style.display = "none";
+  music.pause()
 }
 
 function loseMsg() {
-  msg.innerHTML = `YOU LOSE! You missed ${miss.innerHTML} times!`;
+  msg.innerHTML = `YOU LOSE! Missed ${miss.innerHTML} times!`;
   timeStart = 2;
+  loser.play()
 
 }
 
@@ -84,6 +90,7 @@ function hit(evt) {
   var marker = evt.target;
   if (marker == marker) {
     points.innerHTML = ++points.innerHTML;
+    damage();
     target1.style.display = "none"
 
   }
@@ -100,7 +107,7 @@ function noHit(evt) {
   if (marker !== target1) {
     miss.innerHTML = ++miss.innerHTML;
   }
-  if (miss.innerHTML >= 5) {
+  if (miss.innerHTML >= 3) {
     loseMsg()
     points.innerHTML = 0;
     timer1.innerHTML = timeStart;
@@ -112,6 +119,7 @@ function noHit(evt) {
 
 // button to start 
 function render() {
+  music.play();
   points.innerHTML = 0;
   miss.innerHTML = 0;
   var startPop = setInterval(function () {
@@ -120,14 +128,14 @@ function render() {
       clearInterval(startPop)
     }
     popUp()
-  }, 900)
+  }, 800)
   var endPop = setInterval(function () {
     if (timeStart == 0) {
       target1.style.display = "none"
       clearInterval(endPop)
     }
     removePopUp()
-  }, 700)
+  }, 500)
   if (timer1.innerHTML <= 0) {
     loseMsg()
   }
@@ -144,10 +152,16 @@ function gunSound() {
   gunShot.currentTime = 0;
 
 }
+function damage() {
+  hitSound.play();
+  hitSound.currentTime = 0;
+  grunt.play();
+  grunt.currentTime = 0;
+}
 
 
 function init() {
   target1.style.display = 'none';
   msg.innerHTML = "THE STREETS ARE MEAN";
-
+  
 }
